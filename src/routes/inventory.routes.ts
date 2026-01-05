14de6065602d353e7def7e31d6420d96c09c8bb4
shahzaibahmadpawar1 <shazi8402@gmail.com>
2026-01-05 09:20:36 +0500

    office user added

--- src/routes/inventory.routes.ts
+++ src/routes/inventory.routes.ts
@@ -52,7 +52,7 @@ router.put('/shifts/:shiftId/daily/readings', authenticate, authorize('SM'), upd
 router.post('/shifts/:shiftId/daily/payment-summary', authenticate, authorize('SM'), savePaymentSummaryData);
 router.post('/shifts/:shiftId/daily/lock', authenticate, authorize('SM'), lockDailyShiftData);
 router.patch('/nozzles/:nozzleId/opening-reading', authenticate, authorize('Admin'), updateNozzleOpeningReadingData);
-router.get('/admin/stats', authenticate, authorize('Admin'), getAdminStats);
+router.get('/admin/stats', authenticate, authorize('Admin', 'OU'), getAdminStats);
 router.get('/stations/:stationId/stats', authenticate, getStationStatsData);
 
 export default router;
